58f49cf23bcfe14bbd67eaa6ef537c188c645b0a
The-Shinshi <155411903+The-Shinshi@users.noreply.github.com>
2024-08-10 08:08:50 +0530

    Delete OS directory

--- OS/EXP 1/exp1c.c
+++
@@ -1,9 +0,0 @@
-/* execv system call */ 
-#include<stdio.h> 
-#include<sys/types.h> 
-main(int argc,char *argv[]) 
-{ 
-printf("before execv\n"); 
-execv("/bin/ls",argv); 
-printf("after execv\n"); 
-} 
